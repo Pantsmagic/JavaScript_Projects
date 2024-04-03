567b98309b3599ee2d738085963612e015e000e3
Daniel Ritscher <Daniel.R3@live.de>
2024-04-03 22:57:47 +0200

    add Ternary tests

---
+++ Ternary operator/index.js
@@ -0,0 +1,21 @@
+let age = 21
+let message = age >= 18 ? "You are a adult" : "You are still young"
+
+console.log(message)
+
+let time = 16
+let greeting = time < 12 ? "good morning" : "good afternoon"
+console.log(greeting)
+
+let student = true
+
+let result = student ? "You are a student" : "You are not student"
+console.log(result)
+
+let purchaseAmount = 110
+
+let x = purchaseAmount > 100 ? "You get a 10% discount" : "You get no discount"
+
+console.log(x)
+
+
